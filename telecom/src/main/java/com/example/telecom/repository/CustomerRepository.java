f15217c80b4815357c936908c32c2e058939b83f
SanthoshGuntamadugu <santhoshguntamadugu07@gmail.com>
2024-06-09 18:44:41 +0530

    task5

---
+++ telecom/src/main/java/com/example/telecom/repository/CustomerRepository.java
@@ -0,0 +1,8 @@
+package com.example.telecom.repository;
+
+import com.example.telecom.entity.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Long> {}
+
+
